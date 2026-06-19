7fe720aa899073952cf12ffc36cd973dc9c8f63a
Ankita <ankita91056@gmail.com>
2026-06-19 11:24:28 +0530

    Add files via upload

---
+++ Day-17/common.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[50], b[50];
+    int n1, n2, i, j;
+
+    printf("enter size of first array: ");
+    scanf("%d", &n1);
+
+    printf("enter elements:\n");
+    for(i = 0; i < n1; i++)
+    scanf("%d", &a[i]);
+
+    printf("enter size of second array: ");
+    scanf("%d", &n2);
+
+    printf("enter elements:\n");
+    for(i = 0; i < n2; i++)
+    scanf("%d", &b[i]);
+
+    printf("common elements: ");
+
+    for(i = 0; i < n1; i++)
+    {
+        for(j = 0; j < n2; j++)
+        {
+            if(a[i] == b[j])
+        {
+            printf("%d ", a[i]);
+            break;
+        }
+    }
+}
+return 0;
+}
