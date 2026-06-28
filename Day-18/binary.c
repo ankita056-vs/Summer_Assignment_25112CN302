4d1d76caa3f47debb7b5864469a807b88aba7acf
Ankita <ankita91056@gmail.com>
2026-06-28 13:12:40 +0530

    Add files via upload

---
+++ Day-18/binary.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100], n, i, key;
+    int low, high, mid;
+
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("enter sorted elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("enter elements to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(a[mid] == key)
+        {
+            printf("elements found at position %d", mid + 1);
+            return 0;
+        }
+        else if(a[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        { 
+            high = mid - 1;
+        }
+    }
+    printf("elements not found");
+
+    return 0;
+}
+
+
+
+
+
